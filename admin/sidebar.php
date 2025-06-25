82e5f0a9f7e67d12290c8221b6bfcad3fa1286e0
Lunasans <rene@rene-neuhaus.eu>
2025-06-25 15:47:20 +0200

    Bugfix

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,11 +1,10 @@
 <aside class="sidebar bg-dark text-white p-3" style="width: 220px; min-height: 100vh;">
-  <h4 class="text-white mb-4">🎛️ Admin</h4>
+  <h4 class="text-white mb-4">Admin-Panel</h4>
   <ul class="nav flex-column">
     <li><a href="?page=dashboard">Dashboard</a></li>
     <li><a href="?page=users">Benutzer</a></li>
     <li><a href="?page=settings">Einstellungen</a></li>
     <li><a href="?page=import">Film Import</a></li>
-    <li><a href="?page=phpinfo">PHP Info</a></li>
     <li><a href="logout.php">Logout</a></li>
    </ul>
 </aside>
